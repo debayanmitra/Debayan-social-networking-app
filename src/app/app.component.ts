b6c3703854a7b1ffacdde5462fcd9e0b6eb48601
debayan.mitra <debayan.mitra@accenture.com>
2020-10-05 10:55:51 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'mean-course';
+  title = "mean-course";
 }
