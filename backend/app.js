c9e9a37483ead407c082875389da14095aac2d31
debayan.mitra <debayan.mitra@accenture.com>
2020-10-08 02:14:22 -0500

    Installed mongo

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,33 @@
 const { NgModel } = require("@angular/forms");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const post = require("./model/post");
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, x-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  //code accept post request
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully",
+  });
+  next();
+});
 
 app.use("/api/posts", (req, res, next) => {
   //send some data back
